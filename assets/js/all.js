ac685f9dee3817ae7ab3125c18904a16df6c3209
董聿凌 <gb2013429@gmail.com>
2022-09-10 16:47:45 +0800

    Update 2022-09-10T08:47:40.975Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,20 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
 //# sourceMappingURL=all.js.map
