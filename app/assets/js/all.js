2f2d30885982af308d1a0f8dba1c2830941590cf
董聿凌 <gb2013429@gmail.com>
2022-09-10 16:46:46 +0800

    artist+swipper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,20 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
