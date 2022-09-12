8081b97f5f8fa6cf9c0b891e21990d6f472b7a30
董聿凌 <gb2013429@gmail.com>
2022-09-12 20:48:43 +0800

    jquery-2

--- app/assets/js/jquery.js
+++ app/assets/js/jquery.js
@@ -7,17 +7,13 @@ $('.search').on("click", function (e) {
     $('.search').hide();
     $('.navbar-menu').hide();
     $('.search-active').show();
+    $('.text-search').show();
     // $(this).find('h4').toggleClass('active');
     // $(this).find('.y').toggle();
 })
-$('.search2').on("click", function (e) {
-       $(this).siblings('.text-search').toggle();
-       $('.search-active').show();
-    // $(this).find('h4').toggleClass('active');
-    // $(this).find('.y').toggle();
-})
-$('.text-search').on("click", function (e) {
+$('.close-img').on("click", function (e) {
     $('.search-active').hide();
+    // $('.search-active').hide();
     $('.navber-logo').show();
     $('.search').show();
     $('.navbar-menu').show();
