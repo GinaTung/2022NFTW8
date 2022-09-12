f3fd1c64bccd9050fb9afeccc3694bce5d08bfec
董聿凌 <gb2013429@gmail.com>
2022-09-12 20:49:06 +0800

    Update 2022-09-12T12:49:02.456Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4074,16 +4074,13 @@ $('.search').on("click", function (e) {
   $('.navber-logo').hide();
   $('.search').hide();
   $('.navbar-menu').hide();
-  $('.search-active').show(); // $(this).find('h4').toggleClass('active');
+  $('.search-active').show();
+  $('.text-search').show(); // $(this).find('h4').toggleClass('active');
   // $(this).find('.y').toggle();
 });
-$('.search2').on("click", function (e) {
-  $(this).siblings('.text-search').toggle();
-  $('.search-active').show(); // $(this).find('h4').toggleClass('active');
-  // $(this).find('.y').toggle();
-});
-$('.text-search').on("click", function (e) {
-  $('.search-active').hide();
+$('.close-img').on("click", function (e) {
+  $('.search-active').hide(); // $('.search-active').hide();
+
   $('.navber-logo').show();
   $('.search').show();
   $('.navbar-menu').show(); // $(this).find('h4').toggleClass('active');
