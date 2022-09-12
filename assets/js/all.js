da6d20949bbfa02106c7fd4f3722f8e7314a787d
董聿凌 <gb2013429@gmail.com>
2022-09-12 20:34:13 +0800

    Update 2022-09-12T12:34:09.227Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4065,4 +4065,28 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
   }, "undefined" == typeof e && (C.jQuery = C.$ = S), S;
 });
 "use strict";
+
+$(function () {
+  $('.text-search').hide();
+  $('.search-active').hide();
+});
+$('.search').on("click", function (e) {
+  $('.navber-logo').hide();
+  $('.search').hide();
+  $('.navbar-menu').hide();
+  $('.search-active').show(); // $(this).find('h4').toggleClass('active');
+  // $(this).find('.y').toggle();
+});
+$('.search2').on("click", function (e) {
+  $(this).siblings('.text-search').toggle();
+  $('.search-active').show(); // $(this).find('h4').toggleClass('active');
+  // $(this).find('.y').toggle();
+});
+$('.text-search').on("click", function (e) {
+  $('.search-active').hide();
+  $('.navber-logo').show();
+  $('.search').show();
+  $('.navbar-menu').show(); // $(this).find('h4').toggleClass('active');
+  // $(this).find('.y').toggle();
+});
 //# sourceMappingURL=all.js.map
